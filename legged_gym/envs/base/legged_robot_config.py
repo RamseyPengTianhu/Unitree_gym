9e9dcfab569f2930227718cb757f3eb96dcfb5ad
LeedsRamseyPeng <mntp@leeds.ac.uk>
2025-01-22 12:04:02 +0000

    update

--- legged_gym/envs/base/legged_robot_config.py
+++ legged_gym/envs/base/legged_robot_config.py
@@ -13,7 +13,7 @@ class LeggedRobotCfg(BaseConfig):
 
     class terrain:
         evaluation_mode = False  # if True: evaluate the trained policy
-        mesh_type = 'plane' # "heightfield" # none, plane, heightfield or trimesh
+        mesh_type = 'trimesh' # "heightfield" # none, plane, heightfield or trimesh
         horizontal_scale = 0.1 # [m]
         vertical_scale = 0.005 # [m]
         border_size = 25 # [m]
